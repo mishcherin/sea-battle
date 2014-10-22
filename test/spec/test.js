83916f8b7903f3e59f493825e8d4bcfea61dde5f
daniil.mischerin <daniil.mischerin@corp.mail.ru>
2014-10-22 18:58:48 +0400

    initial commit  o___O

---
+++ test/spec/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function () {
+    describe("A spec", function() {
+        it("teeeeeeeest", function() {
+            expect(true).toEqual(true);
+            console.log('hurrraaa!');
+        });
+    });
+
+})();
